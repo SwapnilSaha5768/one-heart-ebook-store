d858141491948eef4979341676da482109a7647a
SwapnilSaha5768 <sahaswapnil5768@gmail.com>
2025-12-03 17:39:43 +0600

    Make email settings optional for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -195,8 +195,8 @@ EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
 
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="no-reply@oneheartbd.com")
 EMAIL_LOGO_URL = env("EMAIL_LOGO_URL", default="")
