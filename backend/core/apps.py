b82530aec82594fbcfaade812d171dbf60e6e582
SwapnilSaha5768 <sahaswapnil5768@gmail.com>
2025-12-03 18:50:25 +0600

    Fix production 500 error: Set search_path to public on connection created

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.db.backends.signals import connection_created
 
+def set_search_path(sender, connection, **kwargs):
+    if connection.vendor == 'postgresql':
+        with connection.cursor() as cursor:
+            cursor.execute("SET search_path TO public;")
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        connection_created.connect(set_search_path)
