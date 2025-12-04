ea7ed09d7f81d266ef18ed3521e01cd71fe5644e
SwapnilSaha5768 <sahaswapnil5768@gmail.com>
2025-12-05 01:08:09 +0600

    Update debug view

--- backend/core/debug_views.py
+++ backend/core/debug_views.py
@@ -1,12 +1,24 @@
 from django.http import JsonResponse
 from django.conf import settings
 import os
+from blog.models import Post
 
 def debug_settings(request):
+    latest_post = Post.objects.first()
+    image_info = "No posts found"
+    if latest_post and latest_post.featured_image:
+        image_info = {
+            "name": latest_post.featured_image.name,
+            "url": latest_post.featured_image.url,
+            "storage": latest_post.featured_image.storage.__class__.__name__,
+            "backend_storage": settings.DEFAULT_FILE_STORAGE
+        }
+
     return JsonResponse({
         "DEBUG": settings.DEBUG,
         "USE_CLOUDINARY_ENV": os.environ.get("USE_CLOUDINARY"),
         "DEFAULT_FILE_STORAGE": getattr(settings, "DEFAULT_FILE_STORAGE", "Not Set"),
         "INSTALLED_APPS_HAS_CLOUDINARY": "cloudinary" in settings.INSTALLED_APPS,
         "MEDIA_URL": settings.MEDIA_URL,
+        "LATEST_IMAGE_INFO": image_info
     })
